e1da8a179a9f221b3c3caf1e2664a1f0996e5452
Ennio <ennioh@icloud.com>
2024-04-03 13:11:19 -0400

    feat: Rota /GET criada para recuperar as credenciais de um attendee através da função  get-attendee-badge

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import {serializerCompiler,validatorCompiler,ZodTypeProvider} from 'fastify-type
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 
 const app = fastify()
@@ -14,6 +15,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
+app.register(getAttendeeBadge)
 
 
 app.listen({port:3333}).then(()=> {
