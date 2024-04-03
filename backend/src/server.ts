99747bae802fef5c0bfe3f3ed50c01825c7beff9
Ennio <ennioh@icloud.com>
2024-04-03 15:52:45 -0400

    feat(check-in): Rota /GET criada para o participante realizar o check-in no evento

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-ins";
 
 
 const app = fastify()
@@ -16,6 +17,7 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(checkIn)
 
 
 app.listen({port:3333}).then(()=> {
