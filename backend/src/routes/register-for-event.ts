41353ba6aff83232fce28b1bdeb92ee15fedf88c
Ennio <ennioh@icloud.com>
2024-04-03 21:15:24 -0400

    feat(app): API finalizada

--- backend/src/routes/register-for-event.ts
+++ backend/src/routes/register-for-event.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import {z} from "zod";
 import { prisma } from "../lib/prisma";
+import { BadRequest } from "./_errors/bad-request";
 
 export async function registerForEvent(app:FastifyInstance){
     app.withTypeProvider<ZodTypeProvider>().post('/events/:eventId/attendees',{
@@ -37,7 +38,7 @@ export async function registerForEvent(app:FastifyInstance){
         })
 
         if(attendeeFromEmail !== null){
-            throw new Error ('This e-mail is already registered for this event.')
+            throw new BadRequest ('This e-mail is already registered for this event.')
         }
 
 
@@ -56,7 +57,7 @@ export async function registerForEvent(app:FastifyInstance){
 
 
         if(event?.maximumAttendees && amountOfAttendeeForEvent >= event?.maximumAttendees){
-            throw new Error('The maximun number of attendees for this event has been reached.')
+            throw new BadRequest('The maximun number of attendees for this event has been reached.')
 
         }
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,17 +2,24 @@ import fastify from "fastify";
 
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
-import {serializerCompiler, validatorCompiler, jsonSchemaTransform} from 'fastify-type-provider-zod'
+import {serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider} from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-ins";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
 
-const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+
+app.register(fastifyCors,{
+    origin: '*',
+})
 
 app.register(fastifySwagger, {
     swagger: {
@@ -43,7 +50,9 @@ app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
 
+app.setErrorHandler(errorHandler)
 
-app.listen({port:3333}).then(()=> {
+
+app.listen({port:3333, host: '0.0.0.0'}).then(()=> {
     console.log('HTTP server running')
 })
