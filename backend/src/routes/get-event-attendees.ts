ce5fc29507a631aaf2b9ec09728f9fc9b70d31db
Ennio <ennioh@icloud.com>
2024-04-04 12:22:41 -0400

    feat()

--- backend/src/routes/get-event-attendees.ts
+++ backend/src/routes/get-event-attendees.ts
@@ -1,76 +1,91 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import z from "zod";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
+export async function getEventAttendees(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/events/:eventId/attendees', {
+      schema: {
+        summary: 'Get event attendees',
+        tags: ['events'],
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        querystring: z.object({
+          query: z.string().nullish(),
+          pageIndex: z.string().nullish().default('0').transform(Number),
+        }),
+        response: {
+          200: z.object({
+            attendees: z.array(
+              z.object({
+                id: z.number(),
+                name: z.string(),
+                email: z.string().email(),
+                createdAt: z.date(),
+                checkedInAt: z.date().nullable(),
+              })
+            ),
+            total: z.number(),
+          }),
+        },
+      }
+    }, async (request, reply) => {
+      const { eventId } = request.params
+      const { pageIndex, query } = request.query
 
-export async function getEventAttendees(app:FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().get('/events/:eventId/attendees',{
-        schema:{
-            summary: 'Get event attendees',
-            tags:  ['events'],
-            params: z.object({
-                eventId: z.string().uuid(),
-            }),
-            querystring: z.object({
-                query: z.string().nullish(),
-                pageIndex: z.string().nullish().default('0').transform(Number),
-            }),
-            response: {
-                200: z.object({
-                    attendees: z.array(
-                        z.object({
-                            id: z.number(),
-                            name: z.string(),
-                            email: z.string().email(),
-                            createdAt: z.date(),
-                            checkedInAt: z.date().nullable(),
-                        })
-                    )
-                })
-            },
-        }
-    }, async (req, res) => {
-        const { eventId } = req.params;
-        const { pageIndex, query } = req.query;
-
-        const attendees = await prisma.attendee.findMany({
-            select: {
-                id: true,
-                name: true,
-                email: true,
+      const [attendees, total] = await Promise.all([
+        prisma.attendee.findMany({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
+            checkIn: {
+              select: {
                 createdAt: true,
-                checkIn: {
-                    select: {
-                        createdAt:true,
-                    }
-                }
-
-            },
-            where: query ? {
-                eventId,
-                name: {
-                    contains: query
-                }
-            } : {
-                eventId,
-            },
-            take: 10,
-            skip: pageIndex * 10,
-            orderBy: {
-                createdAt: 'desc',
+              }
             }
+          },
+          where: query ? {
+            eventId,
+            name: {
+              contains: query,
+            }
+          } : {
+            eventId,
+          },
+          take: 10,
+          skip: pageIndex * 10,
+          orderBy: {
+            createdAt: 'desc'
+          }
+        }),
+        prisma.attendee.count({
+          where: query ? {
+            eventId,
+            name: {
+              contains: query,
+            }
+          } : {
+            eventId,
+          },
         })
-        return res.send({
-            attendees: attendees.map((attendee => {
-                return {
-                    id: attendee.id,
-                    name: attendee.name,
-                    email: attendee.email,
-                    createdAt: attendee.createdAt,
-                    checkedInAt: attendee.checkIn?.createdAt ?? null,
-                }
-            }))
-        })
-    })    
+      ])
+
+      return reply.send({ 
+        attendees: attendees.map(attendee => {
+          return {
+            id: attendee.id,
+            name: attendee.name,
+            email: attendee.email,
+            createdAt: attendee.createdAt,
+            checkedInAt: attendee.checkIn?.createdAt ?? null,
+          }
+        }),
+        total,
+      })
+    })
 }
